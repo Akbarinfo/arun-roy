1d6859584ded21073500ac853435a445bac0213f
akbarinfo <girtbezbet@gmail.com>
2022-01-19 08:57:26 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 let searchbtn = document.getElementById('id-search');
 let removebtn = document.getElementById('id-remove');
 let modalbox = document.getElementById('id-modalbox');
+let dark = 0;
+
+document.getElementById('id-dark').addEventListener('click', function (){
+  document.body.classList.toggle('dark');
+
+  if (dark==0) {
+  this.textContent = 'light mode';
+  dark++;
+}
+  else {
+    this.textContent = 'dark mode';
+     dark = 0;
+  }
+});
+
 
 searchbtn.addEventListener('click', function() {
   modalbox.classList.add('show');
