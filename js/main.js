624d1a48ae48499903d2ac2d973161d6b8827a94
akbarinfo <girtbezbet@gmail.com>
2022-01-19 23:09:19 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 let searchbtn = document.getElementById('id-search');
 let removebtn = document.getElementById('id-remove');
 let modalbox = document.getElementById('id-modalbox');
-let dark = 0;
-
-document.getElementById('id-dark').addEventListener('click', function (){
-  document.body.classList.toggle('dark');
-
-  if (dark==0) {
-  this.textContent = 'light mode';
-  dark++;
-}
-  else {
-    this.textContent = 'dark mode';
-     dark = 0;
-  }
-});
-
 
 searchbtn.addEventListener('click', function() {
   modalbox.classList.add('show');
@@ -24,3 +9,16 @@ searchbtn.addEventListener('click', function() {
 removebtn.addEventListener('click', function() {
   modalbox.classList.remove('show');
 });
+
+
+// dark light
+
+document.getElementById('id-dark').addEventListener('click', function()
+{
+  document.body.classList.toggle('dark')
+} )
+
+document.getElementById('id-light').addEventListener('click', function()
+{
+  document.body.classList.remove('dark')
+} )
